81439074be65f6e296923cd1102dbd137a1e8ec9
ariannadepasquale <depasqualearianna@gmail.com>
2022-07-13 17:31:42 +0200

    Add test js

--- js/main.js
+++ js/main.js
@@ -6,4 +6,50 @@
  // con il numero della cella cliccata.
 
  
- 
+ //associo il btn ad una variabile
+let btnPlay = document.getElementById('play_btn');
+
+//recupero la griglia dove andranno inseriti i nuovi elementi
+let gridContainer = document.querySelector('.grid_container');
+
+//creo addEventlister sul btn
+btnPlay.addEventListener('click',function(){
+   
+   
+   
+   
+   
+ //creo un ciclo for 
+    for(let i=1 ; i <= 100 ; i++){
+
+        //creo elemento
+        let newBox =document.createElement('div');
+
+        //gli assegno la classe
+        newBox.classList.add('box');
+
+        // dichiaro l'elemento creato
+        newBox.innerHTML = i;
+        console.log(i);
+
+         //importo l'elemento creato
+        gridContainer.append(newBox);
+        
+        //associo la box del dom ad una variabile
+        let box= document.querySelectorAll('.box');
+        console.log (box[i]);
+
+        newBox.addEventListener('click',function(){
+            addToToggleClass (newBox, 'box_active');
+        //stampo in console il numero dell'elemento selezionato
+            console.log(`click su elemento ${i}`);
+        })
+    }
+})
+
+
+//funzione toggle per aggiungere o rimuovere una classe
+function addToToggleClass(elemnt , className){
+    elemnt.classList.toggle(className);
+}
+
